61b3f4689711683629a6476c3047ca0d67aef903
mbnoimi <mbnoimi@gmail.com>
2020-12-02 14:28:11 +0300

    lb4 discover

---
+++ src/models/lists.model.ts
@@ -0,0 +1,56 @@
+import {Entity, model, property} from '@loopback/repository';
+
+@model({settings: {idInjection: false, postgresql: {schema: 'public', table: 'lists'}}})
+export class Lists extends Entity {
+  @property({
+    type: 'string',
+    required: true,
+    id: 1,
+    postgresql: {columnName: 'id', dataType: 'uuid', dataLength: null, dataPrecision: null, dataScale: null, nullable: 'NO'},
+  })
+  id: string;
+
+  @property({
+    type: 'string',
+    required: true,
+    postgresql: {columnName: 'title', dataType: 'character varying', dataLength: null, dataPrecision: null, dataScale: null, nullable: 'NO'},
+  })
+  title: string;
+
+  @property({
+    type: 'string',
+    postgresql: {columnName: 'desc', dataType: 'text', dataLength: null, dataPrecision: null, dataScale: null, nullable: 'YES'},
+  })
+  desc?: string;
+
+  @property({
+    type: 'number',
+    scale: 0,
+    postgresql: {columnName: 'color', dataType: 'smallint', dataLength: null, dataPrecision: null, dataScale: 0, nullable: 'YES'},
+  })
+  color?: number;
+
+  @property({
+    type: 'number',
+    required: true,
+    scale: 0,
+    postgresql: {columnName: 'status', dataType: 'smallint', dataLength: null, dataPrecision: null, dataScale: 0, nullable: 'NO'},
+  })
+  status: number;
+
+  // Define well-known properties here
+
+  // Indexer property to allow additional data
+  // eslint-disable-next-line @typescript-eslint/no-explicit-any
+  [prop: string]: any;
+
+  constructor(data?: Partial<Lists>) {
+    super(data);
+  }
+}
+
+export interface ListsRelations {
+  // describe navigational properties here
+}
+
+export type ListsWithRelations = Lists & ListsRelations;
---
+++ src/models/todos.model.ts
@@ -0,0 +1,55 @@
+import {Entity, model, property} from '@loopback/repository';
+
+@model({settings: {idInjection: false, postgresql: {schema: 'public', table: 'todos'}}})
+export class Todos extends Entity {
+  @property({
+    type: 'string',
+    required: true,
+    id: 1,
+    postgresql: {columnName: 'id', dataType: 'uuid', dataLength: null, dataPrecision: null, dataScale: null, nullable: 'NO'},
+  })
+  id: string;
+
+  @property({
+    type: 'string',
+    required: true,
+    postgresql: {columnName: 'title', dataType: 'character varying', dataLength: null, dataPrecision: null, dataScale: null, nullable: 'NO'},
+  })
+  title: string;
+
+  @property({
+    type: 'string',
+    postgresql: {columnName: 'desc', dataType: 'text', dataLength: null, dataPrecision: null, dataScale: null, nullable: 'YES'},
+  })
+  desc?: string;
+
+  @property({
+    type: 'number',
+    scale: 0,
+    postgresql: {columnName: 'staus', dataType: 'smallint', dataLength: null, dataPrecision: null, dataScale: 0, nullable: 'YES'},
+  })
+  staus?: number;
+
+  @property({
+    type: 'string',
+    required: true,
+    postgresql: {columnName: 'list_id', dataType: 'uuid', dataLength: null, dataPrecision: null, dataScale: null, nullable: 'NO'},
+  })
+  listId: string;
+
+  // Define well-known properties here
+
+  // Indexer property to allow additional data
+  // eslint-disable-next-line @typescript-eslint/no-explicit-any
+  [prop: string]: any;
+
+  constructor(data?: Partial<Todos>) {
+    super(data);
+  }
+}
+
+export interface TodosRelations {
+  // describe navigational properties here
+}
+
+export type TodosWithRelations = Todos & TodosRelations;
